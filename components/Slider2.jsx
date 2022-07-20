a0e81d0ecbe7f0531b1f5b4c5dc02e935d259b06
Edwin Castruita <edwi.castruita117@gmail.com>
2022-07-20 10:05:28 -0500

    Bug Fixes

--- components/Slider2.jsx
+++ components/Slider2.jsx
@@ -7,10 +7,10 @@ export const Slider2 = () => {
     <input type="radio" name="slider" id="s2"/>
     <input type="radio" name="slider" id="s3"/>
     <input type="radio" name="slider" id="s4"/>
-    <label for="s1" id="slide1" style={{borderRadius:"40px"}}></label>
-    <label for="s2" id="slide2"style={{borderRadius:"40px"}}></label>
-    <label for="s3" id="slide3" style={{borderRadius:"40px"}}></label>
-    <label for="s4" id="slide4" style={{borderRadius:"40px"}}></label>
+    <label htmlFor="s1" id="slide1" style={{borderRadius:"40px"}}></label>
+    <label htmlFor="s2" id="slide2"style={{borderRadius:"40px"}}></label>
+    <label htmlFor="s3" id="slide3" style={{borderRadius:"40px"}}></label>
+    <label htmlFor="s4" id="slide4" style={{borderRadius:"40px"}}></label>
   
  </div>
     
