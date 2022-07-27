2f06ed151962eb1920078d2840bbbf53e474699b
Edwin Castruita <edwi.castruita117@gmail.com>
2022-07-27 16:08:18 -0500

    Cambios Generales

--- components/Nosotros.jsx
+++ components/Nosotros.jsx
@@ -33,16 +33,11 @@ const Nosotros = () => {
           </p>
 
           <p className="text-black text-2xl  text-justify pt-36">
-            Somos una empresa mexicana fundada en 1999 en la ciudad de
-            Monterrey, Nuevo León. Contamos con más de 21 años de experiencia
-            brindando soluciones en el sector empresarial de Telecomunicaciones
-            con calidad e innovación.
+            Somos una empresa mexicana fundada en 1999 en la ciudad de Monterrey, Nuevo León. Contamos con más de 21 años de experiencia brindando soluciones en el sector empresarial de Telecomunicaciones con calidad e innovación.
           </p>
 
           <p className="text-black text-2xl pt-10 text-justify">
-            XCIEN instala antenas en torres conocidas como radio bases, donde
-            tiene que existir "linea de vista", es decir, la posibilidad de que
-            dos antenas puedan alinearse de forma directa.
+            XCIEN instala antenas en torres conocidas como radio bases, donde tiene que existir "linea de vista", es decir, la posibilidad de que dos antenas puedan alinearse de forma directa.
           </p>
 <div className="flex justify-end mt-52"><button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
             <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
