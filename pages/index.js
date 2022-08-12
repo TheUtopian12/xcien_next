5aaeadb94df5ed38fabc680bc9657c6b3ed868f6
Edwin Castruita <edwi.castruita117@gmail.com>
2022-08-12 09:18:45 -0500

    Se agrego google maps

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
           href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
           rel="stylesheet"
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
       </Head>
 
