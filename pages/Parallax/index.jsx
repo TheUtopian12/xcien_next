b771876f0f1f4cafd91ab4f549fd5b29abaee742
Edwin Castruita <edwi.castruita117@gmail.com>
2022-08-12 09:23:08 -0500

    Se agrego google maps

--- pages/Parallax/index.jsx
+++ pages/Parallax/index.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import Image from 'next/image'
-import style from '../../styles/Parallax.module.css'
-import mountain from './Mountain.png'
-import trees from './trees.png'
+
 const index = () => {
   return (
-    <div className={style.wrapper}>
-    <header className={style.header}>
-      <Image src={mountain} className={style.background}/>
-      <Image src={trees} className={style.foreground}/>
-      <h1 className={style.title}>Welcome!</h1>
-    </header>
-  </div>
+    <div>index</div>
   )
 }
 
